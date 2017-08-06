4d13dff9aaceafbad687268f4760d6f026870a8d
vinukv-PC\vinu.k.v <vinu.kuriyedath@gmail.com>
2017-08-06 23:17:26 +0530

    Login intial dummy page

---
+++ src/Login.js
@@ -0,0 +1,25 @@
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Button
+} from 'react-native';
+import { StackNavigator } from 'react-navigation';
+
+
+class Login extends React.Component {
+  static navigationOptions = {
+    title: 'Chat with Lucy',
+  };
+  render() {
+    return (
+      <View>
+        <Text>Chat with Lucy</Text>
+      </View>
+    );
+  }
+}
+module.exports = Login;
